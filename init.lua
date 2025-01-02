74d20ea66cee9d797df4cf0e331dffb899d37264
Nick Pankow <nickpankow@gmail.com>
2025-01-02 13:02:59 -1000

    enable clangd lsp

--- init.lua
+++ init.lua
@@ -724,6 +724,8 @@ require('lazy').setup({
           filetypes = { 'ts', 'tsx', 'typescriptreact' },
         },
 
+        clangd = {},
+
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
